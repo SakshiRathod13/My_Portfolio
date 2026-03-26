f0d4cbaf0fc47da5ef5da555d199b01d52447453
SakshiRathod13 <sakshirathod84352@gmail.com>
2026-03-26 22:12:04 +0530

    Spotlight Added

---
+++ my-portfolio/components/prime.tsx
@@ -0,0 +1,17 @@
+import { Spotlight } from 'lucide-react'
+import React from 'react'
+
+
+const prime = () => {
+  return (
+    <div className='pb-20 pt-36'>
+        <div>
+        <Spotlight/>
+        </div>
+
+    </div>
+    
+  )
+}
+
+export default prime
